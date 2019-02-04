b09c74d9cdd8e34da65b7987667f35e3d98c0d32
Siim <siim.pwn@gmail.com>
2019-02-04 08:50:49 +0200

    upload

---
+++ blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from .models import Post
+
+# posts = [
+#     {
+#         'author': 'Siim',
+#         'title': 'Esimene postitus',
+#         'content': 'Esimese postituse sisu',
+#         'date_posted': '1. Jaanuar 2019'
+#     },
+#     {
+#         'author': 'Siim',
+#         'title': 'Teine postitus',
+#         'content': 'Teise postituse sisu',
+#         'date_posted': '10. Jaanuar 2019'
+#     }
+# ]
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
